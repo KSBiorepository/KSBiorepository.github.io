96bb4a94d93e97ca0b4b281617a76e1f90e5a8ff
JackieOlexa <jackie.olexa@umconnect.umt.edu>
2025-01-09 13:07:01 -0600

    some new css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button'
+
+import React from "react";
+import { Navbar } from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import About from "./pages/about";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Hello Jackie!</Button>
-      </header>
+    <div>
+      <title>Kansas State Biorepository</title>
+      <div class='header'><header>Kansas State Biorepository</header></div>
+      <Navbar expand="lg">
+        <Container>
+          <Navbar.Brand href="/"
+            style={{ color: "green" }}>
+            Home page
+          </Navbar.Brand>
+          <Navbar.Brand href="/about"
+            style={{ color: "green" }}>
+            About
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
